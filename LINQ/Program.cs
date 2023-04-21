1ace69d004c253028a1d9bc458ef946184bf74aa
David McGuffey <dmcguffey94@gmail.com>
2023-04-21 18:38:19 -0500

    LINQ videogames

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -2,9 +2,18 @@
 {
     public class Program
     {
+        public static void OrderTitles(string[] titles)
+        {
+            var orderedTitles = titles.OrderBy(title => title.Length);
+            foreach (var title in orderedTitles)
+            {
+                Console.WriteLine(title);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] videoGames = { "God of War", "NBA2K", "Need for Speed", "Legend of Zelda" };
+            OrderTitles(videoGames);
         }
     }
 }
